a6a81fd6a8cfbf76e056d06910b8bf5cbf827b78
unknown <traghav64@gmail.com>
2021-03-05 13:17:42 +0530

    complete quote generator with loader

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,12 +1,34 @@
 // getting all required dom objects
+const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote-text");
 const author = document.getElementById("author-text");
 const newQuoteButton = document.getElementsByClassName("new-quote");
+const twitterBtn = document.getElementById("twitter");
+const loader = document.getElementById("loader");
+const buttonContainer = document.getElementById("button-container");
+
+// defining functions
+
+// loader functions
+
+function load() {
+  quoteContainer.hidden = true;
+  buttonContainer.hidden = true;
+  loader.hidden = false;
+}
+
+function complete() {
+  if (!loader.hidden) {
+    buttonContainer.hidden = false;
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
 // first we get quote from API using async function
 let response, data;
 async function getQuote() {
-  //   let proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  //   let apiUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  load();
   let apiUrl =
     "https://type.fit/api/quotes/?method=getQuote&lang=en&format=json";
   if (!response) {
@@ -15,12 +37,36 @@ async function getQuote() {
   }
   let index = Math.floor(Math.random() * data.length); // generating a random index
   let quote = data[index];
+
+  // changing font size if quote is long
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
   quoteText.textContent = `${quote.text}`;
-  author.textContent = `${quote.author}`;
+
+  // if author is null then show unknown
+  if (quote.author == "null") {
+    author.textContent = `Unknown`;
+  } else {
+    author.textContent = `${quote.author}`;
+  }
+  setTimeout(complete, 200);
 }
 
-getQuote();
+// twitter function
+let tweetQuote = function () {
+  console.log("tweeting");
+  let tweetUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${author.textContent}`;
+  // opening this url in new tab
+  window.open(tweetUrl, "_blank");
+};
 
-newQuoteButton[0].addEventListener("click", function () {
-  getQuote();
-});
+// adding event listeners
+newQuoteButton[0].addEventListener("click", getQuote);
+
+twitterBtn.addEventListener("click", tweetQuote);
+
+// on load
+getQuote();
